4e027c614957310338670808b471b77316cc8004
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2015-10-07 20:22:45 -0300

    Adding validation for params (uid, pub0 and page) and adding specs for this validation

--- lib/app.rb
+++ lib/app.rb
@@ -1,13 +1,21 @@
 require 'sinatra/base'
+require 'sinatra/json'
+require 'utils/string_util'
 
 class FyberOffersApp < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "../templates") }
-  
+
   get '/' do
     erb :home
   end
 
+  before '/offers' do
+    status 400 if params[:uid].blank? || params[:pub0].blank? || params[:page].blank?
+  end
+
   get '/offers' do
+    obj = { foo: 'bar' }
+    json obj
   end
 end
--- spec/lib/app_spec.rb
+++ spec/lib/app_spec.rb
@@ -8,8 +8,39 @@ describe 'Sinatra App' do
     FyberOffersApp.new
   end
 
-  it "displays home page" do
+  it 'displays home page' do
     get '/'
-    expect(last_response.body).to include("Foo")
+    expect(last_response.body).to include('Foo')
+  end
+
+  describe 'GET /offers' do
+    describe 'with errors' do
+      it 'without parameters' do
+        get '/offers'
+        expect(last_response.status).to eq(400)
+      end
+
+      it 'with uid' do
+        get '/offers', uid: '10'
+        expect(last_response.status).to eq(400)
+      end
+
+      it 'with page' do
+        get '/offers', page: '10'
+        expect(last_response.status).to eq(400)
+      end
+
+      it 'with pub0' do
+        get '/offers', pub0: '10'
+        expect(last_response.status).to eq(400)
+      end
+    end
+
+    describe 'with success' do
+      it 'with all parameters' do
+        get '/offers', uid: '10', page: '10', pub0: '10'
+        expect(last_response.status).to eq(200)
+      end
+    end
   end
 end
